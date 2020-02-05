62c18ad46fa7028dd4bc2dde653db98c47665283
jahaziel <jahaziel@TERMINATOR>
2020-02-05 20:40:58 +0100

    Commit marcianito hecho

---
+++ src/codigo/Marciano.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+import java.awt.Image;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author jahaziel
+ */
+public class Marciano {
+
+    public Image imagen1 = null;
+    public Image imagen2 = null;
+    private int anchoPantalla; //El marciano sabe cuanto mide la pantalla
+    public int vida = 50;
+
+    public Marciano(int _anchoPantalla) {
+        anchoPantalla = _anchoPantalla;
+        try {
+            imagen1 = ImageIO.read(getClass().getResource("/imagenes/marcianito1.png"));
+            imagen2 = ImageIO.read(getClass().getResource("/imagenes/marcianito2.png"));
+        } catch (IOException e) { //variable excepción, lo del catch no se  ejecuta
+            System.out.println("ERORRRRRR");
+        }
+
+    }
+}
